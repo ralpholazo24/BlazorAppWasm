9f4cc6f4439649869d7a6357711320ea32210c76
Ralph Olazo <ralpholazo@gmail.com>
2022-09-04 22:15:37 +0200

    Add blazored modal

---
+++ BlazorAppWasm/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppWasm.Models
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
