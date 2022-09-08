2891be6bb5bbf7ae794dfc7dff0744e801557bec
Ralph Olazo <ralpholazo@gmail.com>
2022-09-08 22:04:56 +0200

    Add Category

--- BlazorAppWasm/Models/TodoItem.cs
+++ BlazorAppWasm/Models/TodoItem.cs
@@ -2,8 +2,12 @@
 {
     public class TodoItem
     {
+        public Category Category { get; set; }
+        public Guid CategoryId { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public bool IsDone { get; set; }
     }
+
+    // https://dribbble.com/shots/14100356-ToDo-App-UI
 }
